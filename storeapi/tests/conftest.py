a1fcb870d35c2c500a24db0c3d6ea9e6fe87cb17
Rashed <rashedalamin69@gmail.com>
2026-01-04 03:23:21 +0600

    created post and get tests for out project using AsyncClient in pytest

--- storeapi/tests/conftest.py
+++ storeapi/tests/conftest.py
@@ -1,7 +1,7 @@
 from typing import AsyncGenerator, Generator
 import pytest
-from fastapi.testclient import testclient
-from httpx import AsyncClient
+#from fastapi.testclient import TestClient
+from httpx import AsyncClient,ASGITransport
 
 from storeapi.main import app
 from storeapi.routers.post import post_table, comment_table
@@ -10,16 +10,18 @@ from storeapi.routers.post import post_table, comment_table
 def anyio_backend():
     return "asyncio"
 
-@pytest.fixture()
-def client()->Generator:
-    yield testclient(app)
+# @pytest.fixture()
+# def client()->Generator:
+#     yield TestClient(app)
 
 @pytest.fixture(autouse=True)
 async def db()->AsyncGenerator:
     post_table.clear()
     comment_table.clear()
+    yield
 
 @pytest.fixture()
-async def async_client(client)->AsyncGenerator:
-    async with AsyncClient(app=app,base_url=client.base_url) as ac:
+async def async_client()->AsyncGenerator:
+    async with AsyncClient(transport=ASGITransport(app=app),
+    base_url="http://test") as ac:
         yield ac
